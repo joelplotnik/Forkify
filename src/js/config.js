3286e01a6f7890883a8e4d6c19c66818b2bc9dfa
Joel Plotnik <joelplotnik@gmail.com>
2022-09-12 21:11:49 -0700

    Allow user to upload a recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'ed7fa17c-d766-42a1-b24f-3d3090a0bba2';
+export const MODAL_CLOSE_SEC = 2.5;
